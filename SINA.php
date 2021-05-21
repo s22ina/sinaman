c7462805b0035d3df9a74774849436659e9ebe7d
sina <sinamansouri22@yahoo.com>
2021-05-21 22:21:28 +0430

    kargah

---
+++ SINA.php
@@ -0,0 +1,11 @@
+<?PHP
+
+Name: <input type="text" name="name" value="<?php echo $name;?>">
+
+E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+
+Website: <input type="text" name="website" value="<?php echo $website;?>">
+
+Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+
+?>
